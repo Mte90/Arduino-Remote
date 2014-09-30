2fb35883bb159cd3f7180bbb3a03cb610a9f1c13
Mte90 <mte90smanettator@gmail.com>
2014-10-01 00:26:02 +0200

    now save the ip

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
-    url_green_analog = 'http://' + $('.ip').val() + '/arduino/analog/6/';
-    url_green_digital = 'http://' + $('.ip').val() + '/arduino/digital/6/';
+    var url_green_analog, rl_green_digital, url_red_analog, url_red_digital;
+    //Get ip saved
+    if(localStorage.getItem('ip') !== '') {
+        $( ".ip" ).val(localStorage.getItem('ip'));
+        path($( ".ip" ).val());
+    }
+    //Save the ip on change
+    $( ".ip" ).change(function() {
+        localStorage.setItem('ip', $(this).val());
+        path($(this).val());
+    });
+    //Set the state of checkbox for Green led
     $.get( url_green_digital, function( data ) { 
         $('.led-green').attr('checked', check_bool(data));
             $('.led-green').click(function() {
@@ -12,8 +22,7 @@ $( document ).ready(function() {
                 $.get( url_green_, function( data ) {  });
             });
     });
-    url_red_analog = 'http://' + $('.ip').val() + '/arduino/analog/3/';
-    url_red_digital = 'http://' + $('.ip').val() + '/arduino/digital/3/';
+    //Set the state of checkbox for Red led
     $.get( url_red_digital, function( data ) { 
         $('.led-red').attr('checked', check_bool(data));
             $('.led-red').click(function() {
@@ -25,6 +34,7 @@ $( document ).ready(function() {
                 $.get( url_red_, function( data ) {  });
             });
     });
+    //Notification
     $('.notification').click(function() {
         $.get( url_green_digital, function( data ) { 
             if (Notification.permission !== 'denied') {
@@ -55,12 +65,19 @@ $( document ).ready(function() {
             }
         });
     });
-    
+    //Cute function for check the value on the rest server
     function check_bool(data) {
         if(data !== '0' ) {
             return true;
         } else {
             return false;
         }
+    }
+    //Set the path
+    function path(ip) {
+        url_green_analog = 'http://' + ip + '/arduino/analog/6/';
+        url_green_digital = 'http://' + ip + '/arduino/digital/6/';
+        url_red_analog = 'http://' + ip + '/arduino/analog/3/';
+        url_red_digital = 'http://' + ip + '/arduino/digital/3/';
     }
 });
