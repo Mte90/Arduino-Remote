9cffee05498a47c8d1897c55d719ece6f0aeb1a0
Daniele Scasciafratte <mte90net@gmail.com>
2015-10-02 13:13:06 +0200

    better code, support for localforage, updated jquery

--- js/app.js
+++ js/app.js
@@ -1,35 +1,56 @@
 $(document).ready(function() {
-    var url_custom_digital;
+    var url_digital;
 
     //Get ip saved
-    if(localStorage.getItem('ip') !== '') {
-        $( ".ip" ).val(localStorage.getItem('ip'));
-        path()
-    }
+    localforage.getItem('ip', function(err, value) {
+        if( value !== null && value !== '' ) {
+            $( '.ip' ).val(value);
+            path();
+        } else {
+            $( '.ip' ).val('192.168.0.43');
+            localforage.setItem('ip', $( '.ip' ).val());
+            path();
+        }
+    })
 
     //Get saved custom number
-    if(localStorage.getItem('led-custom-number') !== '') {
-        $( ".led-custom-number" ).val(localStorage.getItem('led-custom-number'));
-        init_pin();
-    }
+    localforage.getItem('led-number', function(err, value) {
+        if( value !== null && value !== '' ) {
+            $( '.led-number' ).val( value );
+            $( '.led-container span' ).html( value );
+            init_pin();
+        } else {
+            $( '.led-number' ).val( '3' );
+            $( '.led-container span' ).html( 3 );
+            init_pin();
+        }
+    });
 
     //Save the ip on change
-    $( ".ip" ).change(function() {
-        localStorage.setItem('ip', $(this).val());
+    $( '.ip' ).change(function() {
+        localforage.setItem( 'ip', $( this ).val());
         path();
         init_pin();
     });
 
     // Save the custom number on change
-    $(".led-custom-number").change(function() {
-        localStorage.setItem('led-custom-number', $(this).val());
+    $('.led-number').change(function() {
+        localforage.setItem( 'led-number', $( this ).val());
+        $( '.led-container span' ).html( $( this ).val() );
         path();
         init_pin();
     });
     
     //Notification
     $('.notification').click(function() {
-        $.get(url_custom_digital, function( data ) { 
+        $.ajax({
+            url: url_digital,
+            dataType: 'json',
+            xhrFields: {
+                mozAnon: true,
+                mozSystem: true
+            }
+        }).done(function( data ) {
             if (Notification.permission !== 'denied') {
                 Notification.requestPermission(function (permission) {
                     if(!('permission' in Notification)) {
@@ -38,7 +59,7 @@ $(document).ready(function() {
                 });
             }
             if (Notification.permission === 'granted') {
-                new Notification('Pin ' + $(".led-custom-number").val() +  ' Led', {
+                new Notification('Pin ' + $( '.led-number' ).val() +  ' Led', {
                     body : 'Pin state: ' + data.return_value
                 });
             }
@@ -46,29 +67,40 @@ $(document).ready(function() {
     });
 
     //Control the custom led
-    $('.led-custom').click(function() {
-        var url_custom_;
-        if($(this).is(':checked'))  {
-            url_custom_ = url_custom_digital + '/1';
+    $( '.led-custom' ).click(function() {
+        var url;
+        if( $(this).is( ':checked' ) )  {
+            url = url_digital + '/1';
         } else {
-            url_custom_ = url_custom_digital + '/0';
+            url = url_digital + '/0';
         }
-        $.get( url_custom_, null);
+        $.ajax({
+            url: url,
+            xhrFields: {
+                mozAnon: true,
+                mozSystem: true
+            }
+        });
     });
     //Set the path
 
     // set the URL for the ON/OFF GET request
     function path() {
-        ip = $('.ip').val();
-        url_custom_digital = 'http://' + ip + '/arduino/digital/' +
-                             $('.led-custom-number').val();
+        ip = $( '.ip' ).val();
+        pin = $( '.led-number' ).val();
+        url_digital = 'http://' + ip + '/arduino/digital/' + pin;
     }
 
     // set OUTPUT mode for the selected pin
     function init_pin() {
-        ip = $('.ip').val();
-        pin = $('.led-custom-number').val();
-        console.log("Init pin" + pin + " on " + ip);
-        $.get('http://' + ip + '/arduino/mode/' + pin + '/o', null)
+        ip = $( '.ip' ).val();
+        pin = $( '.led-number' ).val();
+        $.ajax({
+            url: 'http://' + ip + '/arduino/mode/' + pin + '/o',
+            xhrFields: {
+                mozAnon: true,
+                mozSystem: true
+            }
+        });
     }
 });
