5273412629f0b69ee678480756062420d8a0420e
Daniele Scasciafratte <mte90net@gmail.com>
2015-10-02 13:21:38 +0200

    better code, support for localforage, updated jquery

--- js/app.js
+++ js/app.js
@@ -1,45 +1,82 @@
 $( document ).ready(function() {
-    var url_green_digital, url_red_digital;
+    var url_green_analog, url_green_digital, url_red_analog, url_red_digital;
+    
     //Get ip saved
-    if(localStorage.getItem('ip') !== '') {
-        $( ".ip" ).val(localStorage.getItem('ip'));
-        path($( ".ip" ).val());
-    } else {
-        $( ".ip" ).val('192.168.240.1');
-        path($( ".ip" ).val());
-    }
+    localforage.getItem('ip', function(err, value) {
+        if( value !== null && value !== '' ) {
+            $( ".ip" ).val( value );
+            path( $( ".ip" ).val() );
+        } else {
+            $( ".ip" ).val('192.168.240.1');
+            path( $( ".ip" ).val() );
+        }
+    });
+    
     //Save the ip on change
     $( ".ip" ).change(function() {
-        localStorage.setItem('ip', $(this).val());
-        path($(this).val());
+        localStorage.setItem( 'ip', $( this ).val());
+        path( $(this).val() );
     });
+    
     //Set the state of checkbox for Green led
-    $.get( url_green_digital, function( data ) { 
-        $('.led-green').attr('checked', check_bool(data));
-            $('.led-green').click(function() {
-                if($(this).is(':checked'))  {
-                    url_green_ = url_green_analog + '200';
-                } else {
-                    url_green_ = url_green_analog + '0';
+    $.ajax({
+        url: url_green_digital,
+        xhrFields: {
+            mozAnon: true,
+            mozSystem: true
+        }
+    }).done(function( data ) {
+        $( '.led-green' ).attr( 'checked', check_bool(data));
+        $( '.led-green' ).click(function() {
+            if( $( this ).is( ':checked' ) )  {
+                url_green = url_green_analog + '200';
+            } else {
+                url_green = url_green_analog + '0';
+            }
+            $.ajax({
+                url: url_green,
+                xhrFields: {
+                    mozAnon: true,
+                    mozSystem: true
                 }
-                $.get( url_green_, function( data ) {  });
             });
+        });
     });
+    
     //Set the state of checkbox for Red led
-    $.get( url_red_digital, function( data ) { 
-        $('.led-red').attr('checked', check_bool(data));
-            $('.led-red').click(function() {
-                if($(this).is(':checked'))  {
-                    url_red_ = url_red_analog + '200';
-                } else {
-                    url_red_ = url_red_analog + '0';
+    $.ajax({
+        url: url_red_digital,
+        xhrFields: {
+            mozAnon: true,
+            mozSystem: true
+        }
+    }).done(function( data ) {
+        $( '.led-red' ).attr( 'checked', check_bool(data));
+        $( '.led-red' ).click(function() {
+            if($(this).is(':checked'))  {
+                url_red = url_red_analog + '200';
+            } else {
+                url_red = url_red_analog + '0';
+            }
+            $.ajax({
+                url: url_red,
+                xhrFields: {
+                    mozAnon: true,
+                    mozSystem: true
                 }
-                $.get( url_red_, function( data ) {  });
             });
+        });
     });
+    
     //Notification
     $('.notification').click(function() {
-        $.get( url_green_digital, function( data ) { 
+        $.ajax({
+            url: url_green_digital,
+            xhrFields: {
+                mozAnon: true,
+                mozSystem: true
+            }
+        }).done(function( data ) {
             if (Notification.permission !== 'denied') {
                 Notification.requestPermission(function (permission) {
                     if(!('permission' in Notification)) {
@@ -53,7 +90,13 @@ $( document ).ready(function() {
                 });
             }
         });
-        $.get( url_red_digital, function( data ) { 
+        $.ajax({
+            url: url_red_digital,
+            xhrFields: {
+                mozAnon: true,
+                mozSystem: true
+            }
+        }).done(function( data ) {
             if (Notification.permission !== 'denied') {
                 Notification.requestPermission(function (permission) {
                     if(!('permission' in Notification)) {
@@ -68,6 +111,7 @@ $( document ).ready(function() {
             }
         });
     });
+    
     //Cute function for check the value on the rest server
     function check_bool(data) {
         if(data !== '0' ) {
@@ -76,9 +120,14 @@ $( document ).ready(function() {
             return false;
         }
     }
+    
     //Set the path
     function path(ip) {
+        //Digital for the state
         url_green_digital = 'http://' + ip + '/arduino/digital/6/';
         url_red_digital = 'http://' + ip + '/arduino/digital/3/';
+        //Analog for change the value
+        url_red_analog = 'http://' + ip + '/arduino/analog/3/';
+        url_green_analog = 'http://' + ip + '/arduino/analog/6/';
     }
 });
